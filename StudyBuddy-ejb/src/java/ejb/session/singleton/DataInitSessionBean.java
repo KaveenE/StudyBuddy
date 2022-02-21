9d32ba15bfebd706ab6c6392d0a797a9679c9187
KaveenE <enkaveen999@gmail.com>
2022-02-21 09:28:16 +0000

    Created dummy files to make the folders show up in GitHub

---
+++ StudyBuddy-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.singleton;
+
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author enkav
+ */
+@Singleton
+@LocalBean
+public class DataInitSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
