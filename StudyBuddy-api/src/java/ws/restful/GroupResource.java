0fef2bf068f50b01d836dc882ed5a0bb4882fb62
seancxy <seanchengxy13@gmail.com>
2022-04-09 19:07:45 +0800

    queryparam was accidentally removed

--- StudyBuddy-api/src/java/ws/restful/GroupResource.java
+++ StudyBuddy-api/src/java/ws/restful/GroupResource.java
@@ -23,6 +23,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -52,7 +53,7 @@ public class GroupResource {
         groupEntitySessionBean = new SessionBeanLookup().lookupGroupEntitySessionBeanLocal();
     }
 
-    @Path("retrieveGroupByStudentId/{studentId}")
+    @Path("retrieveGroupsByStudentId/{studentId}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response retrieveGroupsByStudentId(@PathParam("studentId") Long studentId) {
